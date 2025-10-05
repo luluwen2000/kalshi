da4e4381750749d5d97b344e32b51aba5e7af73e
KevinLu <luluwen2000@gmail.com>
2025-10-05 17:20:19 -0500

    pertty print json

--- query.py
+++ query.py
@@ -1,7 +1,8 @@
 import requests
+import json
 
 url = "https://api.elections.kalshi.com/trade-api/v2/events"
 
 response = requests.get(url)
 
-print(response.json())
+print(json.dumps(response.json(), indent=2))
